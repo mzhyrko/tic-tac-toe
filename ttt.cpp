1bcbf785bdd3c7445cad8b2343ee189df1cb9387
Mikhail Zhyrko <mikhail.zhirko@gmail.com>
2024-06-06 18:55:52 +0200

    V1, fixed numeration n*n, added tests

--- ttt.cpp
+++ ttt.cpp
@@ -1,150 +1,253 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-void initBoard(vector<vector<char>> &board, int boardSize) {
-    board.resize(boardSize, vector<char>(boardSize));
+void initBoard(vector<vector<string>> &board, int boardSize) {
+    board = vector<vector<string>>(boardSize, vector<string>(boardSize));
+    int numDigits = to_string(boardSize * boardSize).length();
+
     for (int i = 0; i < boardSize; ++i) {
         for (int j = 0; j < boardSize; ++j) {
-            board[i][j] = char('1' + i * boardSize + j);
+            int num = i * boardSize + j + 1;
+            string numStr = to_string(num);
+            while (numStr.length() < numDigits) {
+                numStr = "0" + numStr; 
+            }
+            board[i][j] = numStr;
         }
     }
 }
 
-void printBoard(const vector<vector<char>> &board, int boardSize) {
+void printBoard(const vector<vector<string>> &board, int boardSize, int lineForWin) {
     for (int i = 0; i < boardSize; ++i) {
         for (int j = 0; j < boardSize; ++j) {
             cout << board[i][j];
             if (j < boardSize - 1) cout << " | ";
         }
         cout << endl;
-        if (i < boardSize - 1) cout << string(boardSize * 4 - 3, '-') << endl;
+        if (i < boardSize - 1) cout << string(boardSize * boardSize - lineForWin, '-') << endl;
     }
 }
 
-bool isValidMove(const vector<vector<char>> &board, int boardSize, int input) {
+bool isValidMove(const vector<vector<string>> &board, int boardSize, int input) {
     int row = (input - 1) / boardSize;
     int col = (input - 1) % boardSize;
-    return input >= 1 && input <= boardSize * boardSize && board[row][col] != 'X' && board[row][col] != 'O';
+    return input >= 1 && input <= boardSize * boardSize && board[row][col] != "X" && board[row][col] != "O";
 }
 
-void makeMove(vector<vector<char>> &board, int boardSize, int input, char currPlay) {
+void makeMove(vector<vector<string>> &board, int boardSize, int input, char currPlay) {
     int row = (input - 1) / boardSize;
     int col = (input - 1) % boardSize;
     board[row][col] = currPlay;
 }
 
 
-bool checkWin(const vector<vector<char>> &board, int boardSize, char currPlay, int lineForWin) {
-    vector<vector<char>> currBoard(lineForWin, vector<char>(lineForWin));
-
-    for(int i = 0; i <= boardSize - lineForWin; i++) {
-        for(int k = 0; k <= boardSize - lineForWin; k++) {
-            for(int m = 0; m < lineForWin; m++) {
-                for(int n = 0; n < lineForWin; n++) {
-                    currBoard[m][n] = board[i + m][k + n];
-                }
-            }
-
-            // Check rows
-            for(int m = 0; m < lineForWin; m++) {
-                int counter = 0;
-                for(int n = 0; n < lineForWin; n++) {
-                    if(currBoard[m][n] == currPlay) {
-                        counter++;
-                        if(counter == lineForWin) return true;
-                    } else {
-                        counter = 0;
-                        break;
-                    }
-                }
-            }
-
-            // Check columns
-            for(int n = 0; n < lineForWin; n++) {
-                int counter = 0;
-                for(int m = 0; m < lineForWin; m++) {
-                    if(currBoard[m][n] == currPlay) {
-                        counter++;
-                        if(counter == lineForWin) return true;
-                    } else {
-                        counter = 0;
-                        break;
-                    }
-                }
-            }
-
-            // Check main diagonal
-            int counter = 0;
-            for(int m = 0; m < lineForWin; m++) {
-                if(currBoard[m][m] == currPlay) {
-                    counter++;
-                    if(counter == lineForWin) return true;
-                } else {
-                    counter = 0;
+bool columnsCheckWin(const vector<vector<string>> &board, int boardSize, char currPlay, int lineForWin, int i, int j){
+    for (int x = 0; x < lineForWin; x++) {
+            bool rowWin = true;
+            for (int y = 0; y < lineForWin; y++) {
+                if (board[i + x][j + y][0] != currPlay) {
+                    rowWin = false;
                     break;
                 }
             }
-
-            // Check anti-diagonal
-            counter = 0;
-            for(int m = 0; m < lineForWin; m++) {
-                if(currBoard[m][lineForWin - 1 - m] == currPlay) {
-                    counter++;
-                    if(counter == lineForWin) return true;
-                } else {
-                    counter = 0;
-                    break;
-                }
-            }
-        }
+        if (rowWin)
+            return true;
     }
     return false;
 }
 
 
-bool isBoardFull(const vector<vector<char>> &board, int boardSize) {
+bool rowsCheckWin(const vector<vector<string>> &board, int boardSize, char currPlay, int lineForWin, int i, int j){
+    for (int y = 0; y < lineForWin; y++) {
+        bool colWin = true;
+        for (int x = 0; x < lineForWin; x++){
+            if (board[i + x][j + y][0] != currPlay) {
+                colWin = false;
+                break;
+            }
+        }
+        if (colWin)
+            return true;
+    }
+    return false;
+}
+
+
+bool mainDiagonalCheckWin(const vector<vector<string>> &board, int boardSize, char currPlay, int lineForWin, int i, int j){
+    bool diagWin1 = true;
+    for (int x = 0; x < lineForWin; x++) {
+        if (board[i + x][j + x][0] != currPlay) {
+            diagWin1 = false;
+            break;
+        }
+    }
+    if (diagWin1)
+        return true;
+    
+    return false;    
+}
+
+
+bool antiDiagonalCheckWin(const vector<vector<string>> &board, int boardSize, char currPlay, int lineForWin, int i, int j){
+    bool diagWin2 = true;
+    for (int x = 0; x < lineForWin; x++) {
+        if (board[i + x][j + lineForWin - 1 - x][0] != currPlay) {
+            diagWin2 = false;
+            break;
+        }
+    }
+    if (diagWin2)
+        return true;
+    return false;
+}
+
+
+bool checkWin(const vector<vector<string>> &board, int boardSize, char currPlay, int lineForWin) {
+    for (int i = 0; i <= boardSize - lineForWin; i++) {
+        for (int j = 0; j <= boardSize - lineForWin; j++) {
+            
+            if(columnsCheckWin(board, boardSize, currPlay, lineForWin, i, j))
+                return true;
+
+            if(rowsCheckWin(board, boardSize, currPlay, lineForWin, i, j))
+                return true;
+
+            if(mainDiagonalCheckWin(board, boardSize, currPlay, lineForWin, i, j))
+                return true;
+
+            if(antiDiagonalCheckWin(board, boardSize, currPlay, lineForWin, i, j))
+                return true;
+        }
+    }
+    return false;
+}
+
+void testsCheckWin(){
+    
+    vector<vector<string>> boardTest;
+    int boardSizeTest;
+    int lineForWinTest;
+    char currPlayTest;
+
+    // Test 1:
+    boardSizeTest = 3;
+    lineForWinTest = 3;
+    currPlayTest = 'X';
+    boardTest = {{"X", "X", "X"},
+                 {"O", "O", "1"},
+                 {"2", "3", "4"}};
+    cout << "Test 1:" << endl;
+    for(int i = 0; i < boardSizeTest; i++){
+        for(int k = 0; k < boardSizeTest; k++){
+            cout << boardTest[i][k] << " ";
+        }
+        cout << endl;
+    }
+    if (checkWin(boardTest, boardSizeTest, currPlayTest, lineForWinTest))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+
+
+    // Test 2:
+    boardSizeTest = 3;
+    lineForWinTest = 3;
+    currPlayTest = 'X';
+    boardTest = {{"1", "2", "X"},
+                 {"3", "X", "1"},
+                 {"X", "3", "4"}};
+    cout << "Test 2:" << endl;
+    for(int i = 0; i < boardSizeTest; i++){
+        for(int k = 0; k < boardSizeTest; k++){
+            cout << boardTest[i][k] << " ";
+        }
+        cout << endl;
+    }
+    if (checkWin(boardTest, boardSizeTest, currPlayTest, lineForWinTest))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+    
+    
+    // Test 3:
+    boardSizeTest = 3;
+    lineForWinTest = 3;
+    currPlayTest = 'X';
+    boardTest = {{"X", "2", "1"},
+                 {"3", "X", "1"},
+                 {"1", "3", "X"}};
+    cout << "Test 3:" << endl;
+    for(int i = 0; i < boardSizeTest; i++){
+        for(int k = 0; k < boardSizeTest; k++){
+            cout << boardTest[i][k] << " ";
+        }
+        cout << endl;
+    }
+    if (checkWin(boardTest, boardSizeTest, currPlayTest, lineForWinTest))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+
+    // Test 4:
+    boardSizeTest = 3;
+    lineForWinTest = 3;
+    currPlayTest = 'X';
+    boardTest = {{"O", "2", "1"},
+                 {"3", "O", "1"},
+                 {"1", "3", "O"}};
+    cout << "Test 4:" << endl;
+    for(int i = 0; i < boardSizeTest; i++){
+        for(int k = 0; k < boardSizeTest; k++){
+            cout << boardTest[i][k] << " ";
+        }
+        cout << endl;
+    }
+    if (checkWin(boardTest, boardSizeTest, currPlayTest, lineForWinTest))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+}
+
+bool isBoardFull(const vector<vector<string>> &board, int boardSize) {
     for (int i = 0; i < boardSize; ++i) {
         for (int j = 0; j < boardSize; ++j) {
-            if (board[i][j] != 'X' && board[i][j] != 'O') return false;
+            if (board[i][j] != "X" && board[i][j] != "O") return false;
         }
     }
     return true;
 }
 
 void changePlayer(char &currPlay) {
-    if(currPlay == 'X')
-    {
+    if(currPlay == 'X'){
         currPlay = 'O';
     }
-    else
-    {
+    else{
         currPlay = 'X';
     }
 }
 
-int main() {
-    
+void game()
+{
     int input;
-    const int boardSize = 4;
-    const int lineForWin = 3;
-    vector<vector<char>> board;
+    int boardSize = 4;
+    int lineForWin = 3;
+    vector<vector<string>> board;
     char currPlay = 'X';
 
     initBoard(board, boardSize);
-    printBoard(board, boardSize);
+    printBoard(board, boardSize, lineForWin);
 
     while (true) {
-        cout << "Player " << currPlay << ":";
+        cout << "Player " << currPlay << ", enter your move: ";
         cin >> input;
 
-        if (isValidMove(board, boardSize, input) == false) {
+        if (!isValidMove(board, boardSize, input)) {
             cout << "Invalid move. Try again." << endl;
             continue;
         }
 
         makeMove(board, boardSize, input, currPlay);
-        printBoard(board, boardSize);
+        printBoard(board, boardSize, lineForWin);
 
         if (checkWin(board, boardSize, currPlay, lineForWin)) {
             cout << "Player " << currPlay << " wins!" << endl;
@@ -158,6 +261,11 @@ int main() {
 
         changePlayer(currPlay);
     }
+}
+
+int main() {
+    
+    testsCheckWin();
 
     return 0;
 }
